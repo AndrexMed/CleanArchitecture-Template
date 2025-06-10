1bf7e83a45750b5c3af1606728cc7b1aa0220046
GiovanniAlzate <giovanniandrex@gmail.com>
2025-06-09 20:45:30 -0500

    Agregar archivos de proyecto.

---
+++ src/Infrastructure/Database/Migrations/20250610014339_alterTableUsers.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations;
+
+/// <inheritdoc />
+public partial class alterTableUsers : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.RenameColumn(
+            name: "password_hash",
+            schema: "dbo",
+            table: "Users",
+            newName: "PasswordHash");
+
+        migrationBuilder.RenameColumn(
+            name: "last_name",
+            schema: "dbo",
+            table: "Users",
+            newName: "LastName");
+
+        migrationBuilder.RenameColumn(
+            name: "first_name",
+            schema: "dbo",
+            table: "Users",
+            newName: "FirstName");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.RenameColumn(
+            name: "PasswordHash",
+            schema: "dbo",
+            table: "Users",
+            newName: "password_hash");
+
+        migrationBuilder.RenameColumn(
+            name: "LastName",
+            schema: "dbo",
+            table: "Users",
+            newName: "last_name");
+
+        migrationBuilder.RenameColumn(
+            name: "FirstName",
+            schema: "dbo",
+            table: "Users",
+            newName: "first_name");
+    }
+}
